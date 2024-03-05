950b07cbd57dcc6ce8238e0a9f485ef87001f277
lindamais <lindalva.ml89@gmail.com>
2024-03-05 17:19:12 -0300

    loja-virtual

---
+++ Módulo 02/07 - Interação com API/Aula 06/Loja Virtual/loja-virtual.js
@@ -0,0 +1,18 @@
+async function procurar(params) {
+    let busca = await fetch("camiseta.json")
+    let produtos = await busca.json()
+    let grupoDiv = document.getElementById("Lista-card")
+    for (let p of produtos){
+        grupoDiv.innerHTML+=`
+            <div class="camiseta">
+            <img src="${p.imagem}" width="auto" height="200">
+            <h3>${p.nome}</h3>
+            <p>R$ ${p.preço}</p>
+            
+        
+
+        </div>   
+    `
+}
+}
+procurar()  
