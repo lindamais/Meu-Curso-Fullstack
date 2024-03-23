bd89fb48077104a343cde5696fb1c28568350140
lindamais <lindalva.ml89@gmail.com>
2024-03-22 23:05:31 -0300

    faltei

--- Módulo 02/07 - Interação com API/Aula 06/Exemplo 04 copy/detalhes.js
+++ Módulo 02/07 - Interação com API/Aula 06/Exemplo 04 copy/detalhes.js
@@ -4,7 +4,18 @@ async function buscarDetalhes(){
 
     //document.body.innertHTML
     let parametros = new URLSearchParams (window.location.search)
-    let parametroID = parametros.get("produto-id") 
-    alert(parametroID)
+    let parametroID = parametros.get("produto-id")
+    
+    let id
+    for(let x in produtos){
+        if (produtos[x].id==parametroID) {
+            id=x
+          
+        }
+    }
+ 
+    document.body.innerHTML=`
+    <img src=${produtos[id].img}>
+    <h1>${produtos[id].nome}</h1>`
 }
 buscarDetalhes()
